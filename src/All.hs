e4e708a0f772b2c848d1f7f28ccce4e2cd6d7682
Guillaume Bouchard <guillaume.bouchard@tweag.io>
2019-12-11 09:59:02 +0100

    Sort tests in All

--- src/All.hs
+++ src/All.hs
@@ -1,6 +1,7 @@
 module All where
 
 import Utils
+import qualified Data.Text as Text
 
 import Day1
 import Day2
@@ -11,9 +12,14 @@ import Day6
 import Day7
 import Day8
 import Day9
+import Day10
+
+allDays = sortBy (comparing (unsafeRead @Int . Text.pack . drop 3 . fst)) $(thisModuleName)
 
 -- Test all samples and return a timing for all
-tests = hspec $ mapM_ (\(name, s) -> describe name s) $(thisModuleName)
+tests = hspec $ mapM_ (\(name, s) -> describe name s) $ allDays
 
 -- Test all samples and return a timing for each
-tests' = mapM_ (\(name, s) -> hspec $ describe name s) $(thisModuleName)
+tests' = mapM_ (\(name, s) -> hspec $ describe name s) $ allDays
+
+-- Test all samples and return a timing for each
